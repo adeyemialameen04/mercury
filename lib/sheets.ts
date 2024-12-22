97c0f09f749c35ea07d22bc1589b65957f01b41d
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-12-22 17:54:47 +0100

    feat: created importing of wallet using seedphrases

--- lib/sheets.ts
+++ lib/sheets.ts
@@ -1,8 +1,10 @@
 import { RouteDefinition } from "react-native-actions-sheet";
 import { registerSheet, SheetDefinition } from "react-native-actions-sheet";
+import ImportWalletSheet from "~/components/wallet/ImportWalletSheet";
 import { WalletSheetWithRouter } from "~/components/wallet/WalletSheetWithRouter";
 
 registerSheet("wallet-sheet-with-router", WalletSheetWithRouter);
+registerSheet("import-wallet-sheet", ImportWalletSheet);
 
 declare module "react-native-actions-sheet" {
 	interface Sheets {
@@ -16,6 +18,7 @@ declare module "react-native-actions-sheet" {
 				}>;
 			};
 		}>;
+		"import-wallet-sheet": SheetDefinition;
 	}
 }
 
